4342c4875d1c7a57c93846864c6338e05d4f1b62
ranggakorompis <korompisrangga14@gmail.com>
2023-05-08 16:40:11 +0800

    login form done

--- src/CoreComponent.js
+++ src/CoreComponent.js
@@ -1,10 +1,11 @@
 import {View, Text} from 'react-native';
 import React from 'react';
+import LoginForm from './components/LoginForm/LoginForm';
 
 const CoreComponent = () => {
   return (
     <View>
-      <Text>Hello world!</Text>
+      <LoginForm />
     </View>
   );
 };
